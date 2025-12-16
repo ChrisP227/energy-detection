3b7e1d8824a9d888f910025a360f1cce36636b24
Christian Paulik <cpaulik13@gmail.com>
2025-12-16 16:50:27 -0600

    Updates dataset.yaml

--- generate_data.sh
+++ generate_data.sh
@@ -4,14 +4,14 @@ YAML=dataset.yaml
 
 # Generating training dataset
 ROOT=dataset/train
-NUMSPEC=1000
+NUMSPEC=10000
 SEED=13
 
 python create_dataset.py $ROOT -y $YAML -n $NUMSPEC -s $SEED --multithread
 
 # Generate validation dataset
 ROOT=dataset/validate
-NUMSPEC=100
+NUMSPEC=1000
 SEED=42
 
 python create_dataset.py $ROOT -y $YAML -n $NUMSPEC -s $SEED --multithread
